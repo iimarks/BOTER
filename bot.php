ca60784336784bac00beae5f41bdc3b9b0f24e30
MARK <37331833+iimarks@users.noreply.github.com>
2018-04-07 18:35:16 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,48 @@
 <?php
 
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+}
+include 'madeline.php';
 
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+class EventHandler extends \danog\MadelineProto\EventHandler
+{
+    public function __construct($MadelineProto)
+    {
+        parent::__construct($MadelineProto);
+    }
+    public function onUpdateNewChannelMessage($update)
+    {
+        $this->onUpdateNewMessage($update);
+    }
+    public function onUpdateNewMessage($update)
+    {
+        if (isset($update['message']['out']) && $update['message']['out']) {
+            return;
+        }
+        if (isset($update['message']['data']) && $update['message']['date'] + 5 < time()) {
+            return;
+        }
+
+        $message = isset($update['message']['message']) ? $update['message']['message'] : '';
+       
+
+
+try {
+  if ($message=="بوت"){
+      $this->messages->sendMessage([
+      "peer"=>$update,
+      "message"=>"البوت يعمل ... ✅",
+      "parse_mode"=>"Markdown",
+      ]);
+  }
+} catch (Exception $e) {
+    echo$e->getMessage();
+    }
+  }
+}
+
+$MadelineProto->start();
+$MadelineProto->setEventHandler('\EventHandler');
+$MadelineProto->loop();
